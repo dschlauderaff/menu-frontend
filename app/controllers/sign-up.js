7e90561e1408d44d26e82059b0fa630c2ef7f31b
Dave Schlauderaff <dschlauderaff@inspirus.com>
2018-10-05 10:11:31 -0500

    form validation and error text added sign-up

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -1,17 +1,26 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
   session: service(),
   sessionAccount: service(),
   ajax: service(),
 
+  showErrors: computed('_showErrors', {
+    get() {
+      return this._showErrors || { email: false, password: false };
+    },
+    set(key, value) {
+      this.set('_showErrors', value);
+      return this._showErrors;
+    }
+  }),
+
   actions: {
     async signUp(event) {
       event.preventDefault();
-      let { email, password } = this;
-      let user = this.store.createRecord('user', { email, password });
-      await user.save();
+      await this.model.save();
       await this.get('session').authenticate('authenticator:oauth2', email, password)
         .then(() => {
           this.get('sessionAccount').loadCurrentUser();
